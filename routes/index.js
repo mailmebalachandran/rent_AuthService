bb3132c209dc3a709063c2ee00f279d5c48141e3
BalachandranKanagasundaram <bala.chandran@exelaonline.com>
2020-04-05 04:00:12 -0400

    Worked on decode Toke api and AuthorisationWithRefreshTOken api

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const authController = require('../controller/authController');
-
+const express = require("express");
+const authController = require("../controller/authController");
 
 const router = express.Router();
 
-router.post('/authenticateUser',authController.authenticateUser);
-router.post('/authenticateWithrefreshtoken', authController.authenticateWithRefreshToken);
-router.post('/verifyToken', authController.verifyToken);
-router.post('/decodeToken', authController.decodeToken);
-
+router.post("/authenticateUser", authController.authenticateUser);
+router.post(
+  "/authenticateWithrefreshtoken",
+  authController.authenticateWithRefreshToken
+);
+router.post("/verifyToken", authController.verifyToken);
+router.post("/decodeToken", authController.decodeToken);
 
 module.exports = router;
